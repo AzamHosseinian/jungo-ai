cbc63286d4863f3f8d8fe06b3e0e5a3bdebf06ae
azal <azam.hosseynian@yahoo.com>
2024-09-13 17:16:01 +0330

    Update footer part responsiveness

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,20 +1,20 @@
 const Footer = () => {
   return (
     <footer className="bg-[#FFF8DC] w-full">
-      <div className="w-full mx-auto max-w-screen-2xl p-4 flex flex-col items-center justify-center">
+      <div className="w-full mx-auto max-w-screen-2xl p-6 flex flex-col items-center justify-center">
         {/* Logotype */}
         <img
           src="/public/assets/images/Footer/logotype.svg"
           alt="JungoAI Logo"
-          className="h-[190px] mb-[95px]"
+          className="h-[190px] mb-[95px] lt-827:h-[100px] lt-827:mb-[50px]"
         />
 
         {/* Footer Links */}
-        <ul className="flex space-x-[121px] sm:flex-row">
+        <ul className="flex bt-827:flex-row lt-827:flex-col items-center justify-center bt-827:space-x-[103px] lt-827:space-y-4 w-full bt-827:w-auto">
           <li>
             <a
               href="#contact"
-              className="font-bold text-[24px] font-[MicroExtendFLF] text-black hover:text-black"
+              className="font-bold text-[24px] font-[MicroExtendFLF] text-black hover:text-black lt-827:text-[18px]"
             >
               CONTACT
             </a>
@@ -22,7 +22,7 @@ const Footer = () => {
           <li>
             <a
               href="#about"
-              className="font-bold text-[24px] font-[MicroExtendFLF] text-black hover:text-black"
+              className="font-bold text-[24px] font-[MicroExtendFLF] text-black hover:text-black lt-827:text-[18px]"
             >
               ABOUT
             </a>
@@ -30,7 +30,7 @@ const Footer = () => {
           <li>
             <a
               href="#social-medias"
-              className="font-bold text-[24px] font-[MicroExtendFLF] text-black hover:text-black"
+              className="font-bold text-[24px] font-[MicroExtendFLF] text-black hover:text-black lt-827:text-[18px]"
             >
               SOCIAL MEDIAS
             </a>
