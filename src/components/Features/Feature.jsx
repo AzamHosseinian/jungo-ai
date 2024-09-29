d416d4060fc915aeeb78bacea0e857423c67d107
azal <azam.hosseynian@yahoo.com>
2024-09-30 01:08:45 +0330

    update responsivness and box sizes

--- src/components/Features/Feature.jsx
+++ src/components/Features/Feature.jsx
@@ -33,11 +33,11 @@ function FeaturesGrid() {
 
       {/* Features Grid */}
       <div className="w-full flex justify-center">
-        <div className="grid lt-827:grid-cols-1 bt-827:grid-cols-2  bt-1280:grid-cols-3 lt-1280:gap-4 bt-1280:gap-16 p-8">
+        <div className="grid lt-827:grid-cols-1 bt-827:grid-cols-2 bt-1280:grid-cols-3 lt-1280:gap-4 bt-1280:gap-16 p-8">
           {features.map((feature, index) => (
             <div
               key={index}
-              className="relative w-[394px] h-[271px] bg-black flex items-center justify-center"
+              className="relative w-[318px] h-[230px] bg-black flex items-center justify-center"
             >
               {/* Custom Box Shape */}
               <img
@@ -47,8 +47,8 @@ function FeaturesGrid() {
               />
 
               {/* Content inside the box */}
-              <div className="relative flex flex-col items-center text-center text-[#FFF8DC] font-microExtendFLF">
-                <div className="flex items-start justify-center w-[280px] h-[68px]">
+              <div className="relative flex flex-col items-center text-center text-[#FFF8DC] font-microExtendFLF mt-8">
+                <div className="flex items-start justify-center w-[280px]">
                   {/* Star */}
                   <img
                     src="/public/assets/images/Features/star.svg"
@@ -56,14 +56,14 @@ function FeaturesGrid() {
                     className="w-[19px] h-[19px] mr-2"
                   />
                   <div
-                    className="font-bold text-[30px]"
+                    className="font-bold text-[22px]"
                     dangerouslySetInnerHTML={{ __html: feature.title }}
                   />
                 </div>
                 <img
                   src={feature.image}
                   alt={feature.title}
-                  className="w-[57px] h-[57px] mt-[72px]"
+                  className="w-[57px] h-[57px] mt-[32px]"
                 />
               </div>
             </div>
