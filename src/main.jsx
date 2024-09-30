54b5e50bc16d77a4ef676527d4b3b0a2dd0eb075
azal <azam.hosseynian@yahoo.com>
2024-09-30 13:22:37 +0330

    update images path

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import "../public/assets/fonts/fonts.css";
+import "../assets/fonts/fonts.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
