90bda43b77c335380c13b7a2c022bfe8485dbe23
azal <azam.hosseynian@yahoo.com>
2024-10-06 01:12:43 +0330

    update image motion

--- src/components/Features/Feature.jsx
+++ src/components/Features/Feature.jsx
@@ -40,22 +40,18 @@ function FeaturesGrid() {
     <div className="min-h-screen flex flex-col items-start justify-center mt-[80px]">
       <h1 className="custom-h1">03 /// FEATURES</h1>
       <div className="w-full flex justify-center">
-        <div className="grid lt-827:grid-cols-1 bt-827:grid-cols-2 bt-1280:grid-cols-3 lt-1280:gap-4 bt-1280:gap-16 p-8">
+        <div className="grid lt-827:grid-cols-1 bt-827:grid-cols-2 bt-1280:grid-cols-3 lt-1280:gap-4 bt-1280:gap-16 p-8 ">
           {features.map((feature, index) => (
             <div
               key={index}
-              className="relative w-[318px] h-[230px] bg-black flex items-center justify-center"
+              className="relative w-[318.21px] h-[229.86px] bg-black flex items-center justify-center"
               onMouseEnter={handleMouseEnter}
               onMouseLeave={handleMouseLeave}
             >
               <img
-                src={
-                  isHovered
-                    ? "assets/images/Features/feature-hover.svg"
-                    : "assets/images/Features/feature1.svg"
-                }
+                src="assets/images/Features/feature-before.svg"
                 alt="Custom Box Shape"
-                className="absolute top-0 left-0 w-full h-full"
+                className="absolute top-0 left-0 w-[318.21px] h-[229.86px]"
               />
               <div className="relative flex flex-col items-center text-center text-[#FFF8DC] font-microExtendFLF mt-8">
                 <div className="flex items-start justify-center w-[280px]">
